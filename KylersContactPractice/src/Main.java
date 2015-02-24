f0441424621930d1c4d8a0a6ced7401376dc9e3a
DmiNTriP <dmintrip@gmail.com>
2015-02-24 10:06:39 -0800

    Practice Contacts app in android IDE

---
+++ KylersContactPractice/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.nio.channels.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		ArrayList<Contacts>aContact = new ArrayList();
+		//create scanneds fir user input 
+		Scanner textIn =new Scanner(System.in);
+		Scanner numIn =new Scanner(System.in);
+		//displays the menu options for the user 
+		
+		System.out.println("======== choose 1 for personal contact");
+		System.out.println("======== choose 2 for business contact");
+		System.out.println("======== choose 3 to get your Contacts");
+		System.out.println("======== choose 4 to quit application");
+		
+		//prompt the user for input 
+		System.out.println("======== please select a usable value");
+		
+		
+		
+		
+		
+		}
+}
