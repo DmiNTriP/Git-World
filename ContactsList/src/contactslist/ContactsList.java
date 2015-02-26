e26267693889ad68b1e485759af53bdc02d73044
DmiNTrip <dmintrip@gmail.com>
2015-02-25 23:52:09 -0800

    Added file writing capabilities before start of progrm

--- ContactsList/src/contactslist/ContactsList.java
+++ ContactsList/src/contactslist/ContactsList.java
@@ -8,8 +8,10 @@
  */
 package contactslist;
 
-import java.io.File;
-import java.io.PrintWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -22,7 +24,7 @@ public class ContactsList {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /**
          * @param dValue option chooser for contact list
          *
@@ -44,6 +46,12 @@ public class ContactsList {
             System.out.println("|     3. Display your Contacts                   |");
             System.out.println("| 4.Exit the program                         |");
             System.out.println("|================================================|");
+            Path source =Paths.get("ContactStorage/Contact.txt");
+            Files.createFile(source);
+             System.out.println("Get File Name: " + source + " " + "was created");
+            //print statement for user input
+            System.out.println("Welcome please press a numerical key to start!");
+
             System.out.println("Please input a corresponding numerical value from the above menu: ");
             digitValue = numIn.nextInt();
             /**
@@ -75,91 +83,79 @@ public class ContactsList {
                 //if user enters anything else other than mentioned above 
                 default:
                     System.out.println("invalid selection please try again");
+
             }
         }
     }
 
     private static void addContacts(ArrayList<Contacts> aContact) {
         //creates a file for user output and input
-//        Scanner fileIn = new Scanner(System.in);
-//        System.out.println("Input File: ");
-//        String fileInName = fileIn.next();
-//        System.out.println("Output File: ");
-//        String fileOutName = fileIn.next();
-
-        //Scanner and PrintWriter objects for reading and Writing 
-     
         Scanner textIn = new Scanner(System.in);
         Scanner numIn = new Scanner(System.in);
-       
-       // System.out.println("Enter 1 for Business contact or 2 for Personal contact");
-       int choice = numIn.nextInt();
-        
-            if (choice == 1) {
-                  System.out.println("Enter your First Name: ");
-        String fName = textIn.nextLine();
-        System.out.println("Enter your Last Name: ");
-        String lName = textIn.nextLine();
-        System.out.println("Enter your Address: ");
-        String address = textIn.nextLine();
-        System.out.println("Enter your Phone number in this format xxx-xxx-xxxx");
-        String phoneNum = textIn.nextLine();
-        System.out.println("Enter your e-mail address: ");
-        String eMail = textIn.nextLine();
-                System.out.println("what is your Job Title: ");
-                String jobTitle = textIn.nextLine();
-                System.out.println("what Department do you work in: ");
-                String department = textIn.nextLine();
-                Contacts aBiz = new Business(fName, lName, address, phoneNum, eMail, jobTitle, department);
-                aContact.add(aBiz);
-            } else if (choice == 2) {
-                  System.out.println("Enter your First Name: ");
-        String fName = textIn.nextLine();
-        System.out.println("Enter your Last Name: ");
-        String lName = textIn.nextLine();
-        System.out.println("Enter your Address: ");
-        String address = textIn.nextLine();
-        System.out.println("Enter your Phone number in this format xxx-xxx-xxxx");
-        String phoneNum = textIn.nextLine();
-        System.out.println("Enter your e-mail address: ");
-        String eMail = textIn.nextLine();
-                System.out.println("What is your birthDate: ");
-                String birthDate = textIn.nextLine();
-                Contacts aFriend = new Personal(fName, lName, address, phoneNum, eMail, birthDate);
-                aContact.add(aFriend);
-            } else {
-                System.out.println("You did not enter a valid selection");
-            }
+
+        // System.out.println("Enter 1 for Business contact or 2 for Personal contact");
+        int choice = numIn.nextInt();
+
+        if (choice == 1) {
+            System.out.println("Enter your First Name: ");
+            String fName = textIn.nextLine();
+            System.out.println("Enter your Last Name: ");
+            String lName = textIn.nextLine();
+            System.out.println("Enter your Address: ");
+            String address = textIn.nextLine();
+            System.out.println("Enter your Phone number in this format xxx-xxx-xxxx");
+            String phoneNum = textIn.nextLine();
+            System.out.println("Enter your e-mail address: ");
+            String eMail = textIn.nextLine();
+            System.out.println("what is your Job Title: ");
+            String jobTitle = textIn.nextLine();
+            System.out.println("what Department do you work in: ");
+            String department = textIn.nextLine();
+            Contacts aBiz = new Business(fName, lName, address, phoneNum, eMail, jobTitle, department);
+            aContact.add(aBiz);
+        } else if (choice == 2) {
+            System.out.println("Enter your First Name: ");
+            String fName = textIn.nextLine();
+            System.out.println("Enter your Last Name: ");
+            String lName = textIn.nextLine();
+            System.out.println("Enter your Address: ");
+            String address = textIn.nextLine();
+            System.out.println("Enter your Phone number in this format xxx-xxx-xxxx");
+            String phoneNum = textIn.nextLine();
+            System.out.println("Enter your e-mail address: ");
+            String eMail = textIn.nextLine();
+            System.out.println("What is your birthDate: ");
+            String birthDate = textIn.nextLine();
+            Contacts aFriend = new Personal(fName, lName, address, phoneNum, eMail, birthDate);
+            aContact.add(aFriend);
+           
+        } else {
+            System.out.println("You did not enter a valid selection");
         }
-    
+    }
+
+    /**
+     *
+     * @param aContact
+     */
+    public static void getContacts(ArrayList<Contacts> aContact) {
         
-     /**
-      * 
-      * @param aContact 
-      */   
-  public static void getContacts(ArrayList<Contacts> aContact) {
 //              //creates a file for user output and input
         Scanner input = new Scanner(System.in);
-//        System.out.println("Input File: ");
-//        String fileInName = fileIn.next();
-//        System.out.println("Output File: ");
-//        String fileOutName=fileIn.next();
-//        
-//        //Scanner and PrintWriter objects for reading and Writing 
-//        File inputFile = new File(fileInName);
+
         Scanner numIn = new Scanner(System.in);
+        System.out.println("Completed contact info");
+
+        
         for (int i = 0; i < aContact.size(); i++) {
             System.out.println(i + " " + aContact.get(i).getfName() + " " + aContact.get(i).getlName());
-           }
+        }
         // option used to write to file
-                System.out.println("Please enter the ID Number of the Contact you wish to see: ");
-                int choice = numIn.nextInt();
-                System.out.println(aContact.get(choice).toString());
-                
-                
+        System.out.println("Please enter the ID Number of the Contact you wish to see: ");
+        int choice = numIn.nextInt();
+        System.out.println(aContact.get(choice).toString());
 
 //        Scanner numIn = new Scanner(System.in);
     }
 
 }
-
